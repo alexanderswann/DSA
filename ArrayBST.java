66c4f4cb8ddca36ae4feaaa9691498e7dadea696
AlexanderSwann <32579236+AlexanderSwann@users.noreply.github.com>
2020-09-30 12:14:49 -0400

    update

--- ArrayBST.java
+++ ArrayBST.java
@@ -1,362 +1,289 @@
-public class ArrayBST{
+public class ArrayBST implements BST {
 
-  Integer arr[];
-  public ArrayBST() {
-    arr = new Integer[8];
-    arr[0] = 0;
-  }
-
-  private int Ncase(int i){
-    if(lV(i) > 0 && rV(i) > 0) {//2 case
-      return 2;
-    }else if(lV(i) > 0 && rV(i) <= 0){//left no right
-      return 1;
-    }else if(lV(i) <= 0 && rV(i) > 0){//right no left
-      return 0;
-    }else if(lV(i) <= 0 && rV(i) <= 0){//leaf case
-      return -1;
-    }else{
-      return -3;
-    }
-  }
-
-  private int lV(int i){
-    if((2*i) + 1 < arr.length){
-      if(arr[(2*i) + 1] == null){
-        return 0;
-      }else{
-        return 1;
-      }
-    }else{
-      return -1;
-    }
-  }
-
-  private int rV(int i){
-    if((2*i) < arr.length){
-      if(arr[2*i] == null){
-        return 0;
-      }else{
-        return 1;
-      }
-    }else{
-      return -1;
-    }
-  }
-
-  public boolean insert(int i){
-    if(arr[1] == null){
-      arr[1] = i;
-      arr[0]++;
-      return true;
-    }else{
-      return insert(1, i);
-    }
-  }
-  //currleft = arr[(2 * i) + 1]
-  //currright = arr[2 * i]
-  private boolean insert(int curr, int i){
-    if(arr[curr] == i){
-      return false;
-
-    }else if (i < arr[curr]) {
-
-      if(lV(curr) == 0){// if left exists but is empty
-        arr[(2 * curr) + 1] = i;
-        arr[0]++;
-        return true;
-      }else if(lV(curr) == -1){
-        expand();
-        arr[(2 * curr) + 1] = i;
-        arr[0]++;
-        return true;
-      } else{
-        return insert((2 * curr) + 1, i);
-      }
-
-    }else{
-      if(rV(curr) == 0){
-        arr[2 * curr] = i;
-        arr[0]++;
-        return true;
-      }else if (rV(curr) == -1) {
-        expand();
-        arr[2 * curr] = i;
-        arr[0]++;
-        return true;
-      }else{
-        return insert(2 * curr, i);
-      }
-    }
-  }
-
-  private void expand(){
-    Integer[] arr2 = new Integer[arr.length * 2];
-
-    for(int i = 0; i< arr.length;i++){
-      if(arr[i] != null){
-        arr2[i] = arr[i];
-      }
+    private Integer arr[];
+    public ArrayBST() {
+        arr = new Integer[8];
+        arr[0] = 0;
     }
 
-    arr = arr2;
-
-    return;
-  }
-
-  public String preOrder(){
-    return preOrder(1);
-  }
-
-  private String preOrder(int curr){
-    if(curr >= arr.length || arr[curr] == null){
-      return "";
-    }else{
-      String left = preOrder((2 * curr) + 1);
-      String right = preOrder(2 * curr);
-      return arr[curr]+ " "  + left + right;
-    }
-  }
-
-
-  public String inOrder(){
-    return inOrder(1);
-  }
-
-  private String inOrder(int curr){
-    if(curr >= arr.length || arr[curr] == null){
-      return "";
-    }else{
-      String left = inOrder((2 * curr) + 1);
-      String right = inOrder(2 * curr);
-      return  left + arr[curr]+ " "   + right;
-    }
-  }
-
-  public String postOrder(){
-    return postOrder(1);
-  }
-
-  private String postOrder(int curr){
-    if(curr >= arr.length || arr[curr] == null){
-      return "";
-    }else{
-      String left = postOrder((2 * curr) + 1);
-      String right = postOrder(2 * curr);
-      return  left  + right+ arr[curr] + " " ;
-    }
-  }
-
-  public String toString(){
-    String toRet = "";
-
-    for(int i = 0; i< arr.length;i++){
-      toRet += arr[i] + " ";
+    public boolean isEmpty() {
+        return arr[0] == 0;
     }
 
-    return toRet;
-  }
+    public void expand() {
+        Integer[] arr2 = new Integer[arr.length * 2];
 
-  public boolean isEmpty(){
-    return arr[0] == 0;
-  }
-
-  public int max(){
-    if(arr[1] == null){
-      return Integer.MIN_VALUE;
-    }
-    return max(1);
-  }
-
-  private int max(int curr){
-    if(rV(curr) <= 0){
-      return arr[curr];
-    } else{
-      return max(2 * curr);
-    }
-  }
-
-  public int min(){
-    if(arr[1] == null){
-      return Integer.MAX_VALUE;
-    }
-    return min(1);
-  }
-
-  private int min(int curr){
-    if(lV(curr) <= 0){
-      return arr[curr];
-    } else{
-      return min((2 * curr) + 1);
-    }
-  }
-
-  public int find(int i){
-    if(arr[1] == null){
-      return -1;
-    } else {
-      return find(1, i);
-    }
-  }
-
-  private int find (int curr, int i){
-    if(arr[1] == null){
-      return -1;
-    }else if (arr[curr] == i){
-      return curr;
-    }else{
-      if(i < arr[curr]){
-        if(lV(curr) <= 0){
-          return -1;
-        }else{
-          return find((2 * curr) + 1, i);
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != null) {
+                arr2[i] = arr[i];
+            }
         }
-      }else{
-        if(rV(curr) <= 0){
-          return -1;
-        }else{
-          return find(2 * curr, i);
+
+        arr = arr2;
+
+        return;
+    }
+
+    public void insert(int i) {
+        if (arr[1] == null) {
+            arr[1] = i;
+            arr[0]++;
+            return;
+        } else {
+            insert(1, i);
         }
-      }
     }
-  }
+    //currleft = arr[(2 * i) + 1]
+    //currright = arr[2 * i]
+    private void insert(int curr, int i) {
+        if (arr[curr] == i) {
+            System.out.println(i + " is already in the tree");
+            return;
 
-  public boolean contains(int i){
-    return find(i) >= 0;
-  }
+        } else if (i < arr[curr]) {
 
-  public int findParent(int i){
-    if(arr[1] == null){
-      return -1;
-    }else {
-      return findParent(1, i);
-    }
-  }
+            if (lV(curr) == 0) { // if left exists but is empty
+                arr[(2 * curr) + 1] = i;
+                arr[0]++;
+                return;
+            } else if (lV(curr) == -1) {
+                expand();
+                arr[(2 * curr) + 1] = i;
+                arr[0]++;
+                return;
+            } else {
+                insert((2 * curr) + 1, i);
+            }
 
-  public int findParent(int curr, int i){
-    if(curr >= arr.length || arr[curr] == null){
-      return -1;
-    }else if(rV(curr)>0 && arr[2 * curr] == i){
-      return curr;
-    }else if (lV(curr) > 0 && arr[(2 * curr) +1 ]==i){
-      return curr;
-    }else if (i > arr[curr]){
-      return findParent((2 * curr), i);
-    }else{
-      return findParent(((2 * curr) +1), i);
-    }
-  }
-
-  public String tranm(int i){
-    return arr[i] + "";
-  }
-
-  public void remove(int i){
-
-    int curr = find(i);
-    if (curr == -1) {
-      System.out.println("not in the array");
-      return;
-    }else{
-      if(Ncase(curr) == -1){
-        arr[curr] = null;
-      }else if(Ncase(curr) == 2 || Ncase(curr) == 0){//right and 2 case
-        int pred = max(curr);
-        int data = pred;
-        remove(pred);
-        arr[curr] = data;
-
-        return;
-      }else if(Ncase(curr) == 1){//left
-        int succ = min(curr);
-        int data = succ;
-        remove(succ);
-        arr[curr] = data;
-        return;
-      }else{
-        return;
-      }
+        } else {
+            if (rV(curr) == 0) {
+                arr[2 * curr] = i;
+                arr[0]++;
+                return;
+            } else if (rV(curr) == -1) {
+                expand();
+                arr[2 * curr] = i;
+                arr[0]++;
+                return;
+            } else {
+                insert(2 * curr, i);
+            }
+        }
     }
 
+    public String preOrder() {
+        return preOrder(1);
+    }
+
+    private String preOrder(int curr) {
+        if (curr >= arr.length || arr[curr] == null) {
+            return "";
+        } else {
+            String left = preOrder((2 * curr) + 1);
+            String right = preOrder(2 * curr);
+            return arr[curr] + " " + left + right;
+        }
+    }
+
+    public String postOrder() {
+        return postOrder(1);
+    }
+
+    private String postOrder(int curr) {
+        if (curr >= arr.length || arr[curr] == null) {
+            return "";
+        } else {
+            String left = postOrder((2 * curr) + 1);
+            String right = postOrder(2 * curr);
+            return left + right + arr[curr] + " ";
+        }
+    }
+
+    public String inOrder() {
+        return inOrder(1);
+    }
+
+    private String inOrder(int curr) {
+        if (curr >= arr.length || arr[curr] == null) {
+            return "";
+        } else {
+            String left = inOrder((2 * curr) + 1);
+            String right = inOrder(2 * curr);
+            return left + arr[curr] + " " + right;
+        }
+    }
+
+    public boolean contains(int i) {
+        return find(i) >= 0;
+    }
+    public int find(int i) {
+        if (arr[1] == null) {
+            return -1;
+        } else {
+            return find(1, i);
+        }
+    }
+
+    private int find(int curr, int i) {
+        if (arr[1] == null) {
+            return -1;
+        } else if (arr[curr] == i) {
+            return curr;
+        } else {
+            if (i < arr[curr]) {
+                if (lV(curr) <= 0) {
+                    return -1;
+                } else {
+                    return find((2 * curr) + 1, i);
+                }
+            } else {
+                if (rV(curr) <= 0) {
+                    return -1;
+                } else {
+                    return find(2 * curr, i);
+                }
+            }
+        }
+    }
+
+    public int min() {
+        if (arr[1] == null) {
+            return Integer.MAX_VALUE;
+        }
+        return min(1);
+    }
+
+    private int min(int curr) {
+        if (lV(curr) <= 0) {
+            return arr[curr];
+        } else {
+            return min((2 * curr) + 1);
+        }
+    }
+    public int max() {
+        if (arr[1] == null) {
+            return Integer.MIN_VALUE;
+        }
+        return max(1);
+    }
+
+    private int max(int curr) {
+        if (rV(curr) <= 0) {
+            return arr[curr];
+        } else {
+            return max(2 * curr);
+        }
+    }
+
+    public void delete(int i) {
+
+        int curr = find(i);
+        if (curr == -1) {
+            System.out.println(i + " is not in the tree");
+            return;
+        } else {
+            if (Ncase(curr) == -1) {
+                arr[curr] = null;
+                arr[0]--;
+            } else if (Ncase(curr) == 2 || Ncase(curr) == 0) { //right and 2 case
+
+                int pred = min(2 * curr);
+                int data = pred;
+                delete(pred);
+                arr[curr] = data;
+
+                return;
+            } else if (Ncase(curr) == 1) { //left
+                int sc = max(2 * curr + 1);
+                int data = sc;
+                delete(sc);
+                arr[curr] = data;
+                return;
+            } else {
+                System.out.println("you should not be here");
+                return;
+            }
+        }
+
+    }
+
+    public int findParent(int i) {
+        if (arr[1] == null) {
+            return -1;
+        } else {
+            return findParent(1, i);
+        }
+    }
+
+    public int findParent(int curr, int i) {
+        if (curr >= arr.length || arr[curr] == null) {
+            return -1;
+        } else if (rV(curr) > 0 && arr[2 * curr] == i) {
+            return curr;
+        } else if (lV(curr) > 0 && arr[(2 * curr) + 1] == i) {
+            return curr;
+        } else if (i > arr[curr]) {
+            return findParent((2 * curr), i);
+        } else {
+            return findParent(((2 * curr) + 1), i);
+        }
+    }
+
+    private int Ncase(int i) {
+        if (lV(i) > 0 && rV(i) > 0) { //2 case
+            return 2;
+        } else if (lV(i) > 0 && rV(i) <= 0) { //left no right
+            return 1;
+        } else if (lV(i) <= 0 && rV(i) > 0) { //right no left
+            return 0;
+        } else if (lV(i) <= 0 && rV(i) <= 0) { //leaf case
+            return -1;
+        } else {
+            return -3;
+        }
+    }
+
+    private int lV(int i) {
+        if ((2 * i) + 1 < arr.length) {
+            if (arr[(2 * i) + 1] == null) {
+                return 0;
+            } else {
+                return 1;
+            }
+        } else {
+            return -1;
+        }
+    }
+
+    private int rV(int i) {
+        if ((2 * i) < arr.length) {
+            if (arr[2 * i] == null) {
+                return 0;
+            } else {
+                return 1;
+            }
+        } else {
+            return -1;
+        }
+    }
+
+    public String ind(int i) {
+        if (i < arr.length && i >= 0) {
+            return arr[i] + "";
+        } else {
+            return "index out of bounds";
+        }
+    }
 
 
-  }
+    public String toString() {
+        String toRet = "";
 
+        for (int i = 0; i < arr.length; i++) {
+            toRet += arr[i] + " ";
+        }
 
-    //
-  	// /**
-    //     * Checks whether the tree is empty
-    //     * @return true if the tree is empty, false otherwise
-    //     */
-  	// boolean isEmpty();
-    //
-  	// /**
-    //     * Makes the array twice as large and moves all the data
-    //     * into the bigger array.
-    //     */
-  	// void expand();
-    //
-    //     /**
-    //     * Inserts a new number into the tree
-    //     * @param a number to insert
-    //     */
-    //     void insert(int data);
-    //
-    //     /**
-    //     * Compiles a pre-order traversal as a string
-    //     * @return a pre-order traversal as a string
-    //     */
-    //     String preOrder();
-    //
-    //     /**
-    //     * Compiles a post-order traversal as a string
-    //     * @return a post-order traversal as a string
-    //     */
-    //     String postOrder();
-    //
-    //     /**
-    //     * Compiles a in-order traversal as a string
-    //     * @return a in-order traversal as a string
-    //     */
-    //     String inOrder();
-    //
-    //
-  	// /**
-    //     * Checks whether a number is in the tree
-    //     * @param the number to check
-    //     * @return true if the number is in the tree, false otherwise
-    //     */
-  	// boolean find(int data);
-    //
-  	// /**
-    //     * Finds the index of a particular number in the tree.
-    //     * If it does not exist, return -1
-    //     * @param the number to look for
-    //     * @return the index where it is located, or -1
-    //     */
-  	// int find(int data);
-    //
-  	// /**
-    //     * Finds the minimum of the tree
-    //     * @return the minimum value in the tree
-    //     */
-  	// int min();
-    //
-  	// /**
-    //     * Finds the maximum of the tree
-    //     * @return the maximum value in the tree
-    //     */
-  	// int max();
-    //
-    //
-  	// /**
-    //     * Deletes a number from the tree. DO NOT iterate
-    //     * over the array to find the data. Make sure to
-    //     * maintain the structure of the tree.
-    //     * @param the number to delete
-    //     */
-  	// void delete(int data);
-
-
+        return toRet;
+    }
 
 }
--- BSTTester.java
+++ BSTTester.java
@@ -1,65 +1,81 @@
-public class BSTTester{
-	public static void main(String[] args){
-    ArrayBST arrTree = new ArrayBST();
+public class BSTTester {
+    public static void main(String[] args) {
+        ArrayBST arrTree = new ArrayBST();
 
 
-    //System.out.println(arrTree.isEmpty());
+        //System.out.println(arrTree.isEmpty());
 
-		arrTree.insert(9);
-		arrTree.insert(0);
-		arrTree.insert(3);
-		arrTree.insert(10);
-		arrTree.insert(4);
-		arrTree.insert(2);
-
-
-
-
-
-		System.out.println(arrTree.preOrder());
-
-		System.out.println(arrTree.tranm(arrTree.findParent(4)));
-
-		arrTree.remove(0);
+        arrTree.insert(8);
+        arrTree.insert(10);
+        arrTree.insert(5);
+        arrTree.insert(12);
+        arrTree.insert(11);
+        arrTree.insert(6);
+        arrTree.insert(7);
+        arrTree.insert(7);
 
 
 
-    // System.out.println(arrTree.inOrder());
-    System.out.println(arrTree.preOrder());
-    // System.out.println(arrTree.postOrder());
-
-		System.out.println(arrTree.tranm(arrTree.findParent(4)));
-
-    // System.out.println(arrTree.max());
-    // System.out.println(arrTree.min());
-    //
-    // System.out.println(arrTree.find(4));
-    // System.out.println(arrTree.find(5));
-    //
-    // System.out.println(arrTree.contains(4));
-    // System.out.println(arrTree.contains(5));
-    // System.out.println(arrTree.isEmpty());
-    // System.out.println("index " +arrTree.findParent(4));
-
-		BinarySearchTree tree = new BinarySearchTree();
-
-    tree.add(9);
-    tree.add(0);
-    tree.add(3);
-    tree.add(10);
-    tree.add(4);
-		tree.add(2);
-
-		System.out.println(tree.preOrder());
-
-    System.out.println(tree.findParent(4));
-
-    tree.remove(0);
 
 
-		System.out.println(tree.preOrder());
+        System.out.println(arrTree.preOrder());
 
-		System.out.println(tree.findParent(4));
-	}
+        //System.out.println(arrTree.ind(arrTree.findParent(4)));
+        System.out.println("remove 8");
+        arrTree.delete(8);
+        arrTree.delete(8);
+        System.out.print("parent of 12 is ");
+        System.out.println(arrTree.ind(arrTree.findParent(12)));
+        System.out.print("items in list ");
+        System.out.println(arrTree.ind(0));
+
+        //System.out.println(arrTree);
+
+
+
+
+        System.out.println(arrTree.preOrder());
+        //System.out.println(arrTree.postOrder());
+        //System.out.println(arrTree.inOrder());
+
+        //System.out.println(arrTree.ind(arrTree.findParent(4)));
+
+        // System.out.println(arrTree.max());
+        // System.out.println(arrTree.min());
+        //
+        // System.out.println(arrTree.find(4));
+        // System.out.println(arrTree.find(5));
+        //
+        // System.out.println(arrTree.contains(4));
+        // System.out.println(arrTree.contains(5));
+        // System.out.println(arrTree.isEmpty());
+        // System.out.println("index " +arrTree.findParent(4));
+
+        System.out.println("___________\nTest Against the tree");
+
+        BinarySearchTree tree = new BinarySearchTree();
+
+        tree.add(8);
+        tree.add(10);
+        tree.add(5);
+        tree.add(12);
+        tree.add(11);
+        tree.add(6);
+        tree.add(7);
+
+        System.out.println(tree.preOrder());
+
+        //System.out.println(tree.findParent(4));
+        System.out.println("remove 8");
+        tree.remove(8);
+
+        System.out.print("parent of 12 is ");
+        System.out.println(tree.findParent(12));
+
+
+        System.out.println(tree.preOrder());
+
+        //System.out.println(tree.findParent(4));
+    }
 
 }
