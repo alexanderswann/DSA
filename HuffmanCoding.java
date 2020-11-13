d16113a85b4f8e18546f70e335b78d110166e7bb
AlexanderSwann <32579236+AlexanderSwann@users.noreply.github.com>
2020-11-12 23:03:55 -0500

    update

--- HuffmanCoding.java
+++ HuffmanCoding.java
@@ -1,12 +1,15 @@
 import java.util.*;
+import java.io.*;
 public class HuffmanCoding {
   public static void main(String[] args){
-    String phrase = "hooray weve been state champs";
+    String phrase = "hooray we are state champs";
     HashMap<String, Integer> frequencies = findFrequencies(phrase);
     Set<String> keys = frequencies.keySet();
     for(String key : keys){
       System.out.println(key + ", " + frequencies.get(key));
     }
+
+    System.out.println("-----");
 
     NodeComparator comp = new NodeComparator();
     PriorityQueue<Node> q = new PriorityQueue<Node>(keys.size(), comp);
@@ -16,10 +19,66 @@ public class HuffmanCoding {
       q.add(curr);
     }
 
-    while (q.peek() != null){
-      Node curr = q.poll();
-      System.out.println(curr.letter + ", " + curr.freq);
+    // while (q.peek() != null){
+    //   Node curr = q.poll();
+    //   System.out.println(curr.letter + ", " + curr.freq);
+    // }
+
+    Node curr = makeTree(q);
+
+    FileWriter writer = null;
+    try {
+      writer = new FileWriter("../../codes.txt");
+    }catch (IOException e){
+      System.out.println("Messed up");
+      e.printStackTrace();
     }
+
+    // 0 is left, 1 is right
+    makeCodes(curr, "", writer);
+
+    try {
+      writer.close();
+    } catch(IOException e) {
+      System.out.println("couldnt close");
+      e.printStackTrace();
+    }
+
+
+  }
+  public static void makeCodes(Node curr, String codeSoFar, FileWriter writer){
+    if(curr == null){
+      return;
+    }else if (curr.letter != null){
+      System.out.println(curr.letter + ": " + codeSoFar);
+        try {
+          writer.write(curr.letter + "," + codeSoFar + "\n");
+        }catch(IOException e){
+          System.out.println("cant write");
+        }catch(NullPointerException e2){
+          System.out.println("NullPointerException");
+        }
+    }else{
+      makeCodes(curr.right, codeSoFar + "1", writer);
+      makeCodes(curr.left, codeSoFar + "0", writer);
+    }
+
+  }
+
+  public static Node makeTree(PriorityQueue<Node> q){
+    while(q.size() > 1){
+      Node smaller = q.poll();
+      Node other = q.poll();
+
+      int totalFreq = smaller.freq + other.freq;
+
+      Node parent = new Node(null, totalFreq);
+      parent.right = smaller;
+      parent.left = other;
+      q.add(parent);
+
+    }
+      return q.poll();
   }
 
   public static HashMap<String, Integer> findFrequencies(String phrase){
