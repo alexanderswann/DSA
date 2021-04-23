0b5d59d38c88e9d8a15d80556d2e979c9a6138ce
alexanderswann <32579236+alexanderswann@users.noreply.github.com>
2021-04-23 16:25:32 -0400

    s

--- main.js
+++ main.js
@@ -12,111 +12,109 @@ var datatopsongs = '';
 //   	button.mousePressed(topsongs);
 //
 // }
-function printtop(top){
-  console.log(top);
-  alert("hello")
+function printtop(top) {
+    console.log(top);
+    alert("hello")
 
-  var title = "";
-  var img  = "";
-  var artists   = "";
-
-
-  for (var i = 0; i < top.items.length; i++) {
-    var title = top.items[i].name;
+    var title = "";
+    var img = "";
     var artists = "";
 
-    for(var j = 0; j < top.items[i].artists.length; j++) {
 
-      // artists += top.items[i].artists[j].name + ", ";
-      // console.log(j);
-      // console.log(top.items[i].artists.length);
-      if(j < top.items[i].artists.length -1){
-          artists += top.items[i].artists[j].name + ", ";
+    for (var i = 0; i < top.items.length; i++) {
+        var title = top.items[i].name;
+        var artists = "";
 
-      }else {
-        artists += top.items[i].artists[j].name;
-      }
+        for (var j = 0; j < top.items[i].artists.length; j++) {
 
+            // artists += top.items[i].artists[j].name + ", ";
+            // console.log(j);
+            // console.log(top.items[i].artists.length);
+            if (j < top.items[i].artists.length - 1) {
+                artists += top.items[i].artists[j].name + ", ";
+
+            } else {
+                artists += top.items[i].artists[j].name;
+            }
+
+        }
+        var img = top.items[i].album.images[1].url
+
+
+        songprinter(title, img, artists, i);
+        console.log(title);
+        console.log(img);
+        console.log(artists);
     }
-    var img = top.items[i].album.images[1].url
-
-
-    songprinter(title, img, artists, i);
-    console.log(title);
-    console.log(img);
-    console.log(artists);
-}
 
 }
 
-function songprinter(title, img, artists, i){
-//   var element = document.createElement("div");
-//   element.appendChild(document.createTextNode('The man who mistook his wife for a hat'));
-//   document.getElementById('lc').appendChild(element);
-//
-//
-//   var div = document.createElement('div');
-// div.classList.add('test');
-// var text = document.createTextNode('Test');
-// div.appendChild(text);
-// document.body.appendChild(div)
+function songprinter(title, img, artists, i) {
+    //   var element = document.createElement("div");
+    //   element.appendChild(document.createTextNode('The man who mistook his wife for a hat'));
+    //   document.getElementById('lc').appendChild(element);
+    //
+    //
+    //   var div = document.createElement('div');
+    // div.classList.add('test');
+    // var text = document.createTextNode('Test');
+    // div.appendChild(text);
+    // document.body.appendChild(div)
 
 
-  var list = document.getElementById("list");
-  var container = document.createElement("li");
-  container.classList.add('container');
-  container.setAttribute("id", "container" + i);
-  document.getElementById('list').appendChild(container);
+    var list = document.getElementById("list");
+    var container = document.createElement("li");
+    container.classList.add('container');
+    container.setAttribute("id", "container" + i);
+    document.getElementById('list').appendChild(container);
 
-  var number = document.createElement("div");
-  number.classList.add('number');
-  number.setAttribute("id", "number " + (i +1));
-  number.innerHTML = (i +1);
-  document.getElementById("container" + i).appendChild(number);
+    var number = document.createElement("div");
+    number.classList.add('number');
+    number.setAttribute("id", "number " + (i + 1));
+    number.innerHTML = (i + 1);
+    document.getElementById("container" + i).appendChild(number);
 
-  var imgcontainer = document.createElement("div");
-  imgcontainer.classList.add('img-container');
-  imgcontainer.setAttribute("id", "imgcontainer" + i);
-  document.getElementById("container" + i).appendChild(imgcontainer);
-
-
-
-  var txtcontainer = document.createElement("div");
-  txtcontainer.classList.add('text-container');
-  txtcontainer.setAttribute("id", "txtcontainer" + i);
-  document.getElementById("container" + i).appendChild(txtcontainer);
-
-  var img2 = document.createElement('img');
-  img2.src =img ;
-  img2.classList.add('realimg');
-  document.getElementById("imgcontainer" + i).appendChild(img2);
-
-  var song_name = document.createElement("div");
-  song_name.classList.add('product-info');
-  song_name.setAttribute("id", "song_name" + i);
-  song_name.innerHTML = title;
-  document.getElementById("txtcontainer" + i).appendChild(song_name);
-
-  var artists_name = document.createElement("div");
-  artists_name.classList.add('product-info');
-  artists_name.setAttribute("id", "artists_name" + i);
-  artists_name.innerHTML = artists + "<br>";
-  document.getElementById("txtcontainer" + i).appendChild(artists_name);
-
-  var x = document.createElement("BR");
+    var imgcontainer = document.createElement("div");
+    imgcontainer.classList.add('img-container');
+    imgcontainer.setAttribute("id", "imgcontainer" + i);
+    document.getElementById("container" + i).appendChild(imgcontainer);
 
 
-  list.insertBefore(x, container);
+
+    var txtcontainer = document.createElement("div");
+    txtcontainer.classList.add('text-container');
+    txtcontainer.setAttribute("id", "txtcontainer" + i);
+    document.getElementById("container" + i).appendChild(txtcontainer);
+
+    var img2 = document.createElement('img');
+    img2.src = img;
+    img2.classList.add('realimg');
+    document.getElementById("imgcontainer" + i).appendChild(img2);
+
+    var song_name = document.createElement("div");
+    song_name.classList.add('product-info');
+    song_name.setAttribute("id", "song_name" + i);
+    song_name.innerHTML = title;
+    document.getElementById("txtcontainer" + i).appendChild(song_name);
+
+    var artists_name = document.createElement("div");
+    artists_name.classList.add('product-info');
+    artists_name.setAttribute("id", "artists_name" + i);
+    artists_name.innerHTML = artists + "<br>";
+    document.getElementById("txtcontainer" + i).appendChild(artists_name);
+
+    var x = document.createElement("BR");
 
 
+    list.insertBefore(x, container);
 
 
 
 
 }
 
-function breaks(){
-  var div = document.getElementById("list");
+function breaks() {
+    var div = document.getElementById("list");
 
     // Get all span elements inside of div
     var spans = div.getElementsByClassName("container");
@@ -124,10 +122,10 @@ function breaks(){
     // Create a loop which will insert a br element before each span element in div, starting from the second span element
     var i;
     for (i = 1; i < spans.length; i++) {
-      var newElem = document.createElement("BR");
-      div.insertBefore(newElem, spans[i]);
+        var newElem = document.createElement("BR");
+        div.insertBefore(newElem, spans[i]);
     }
-  }
+}
 
 
 //
@@ -135,87 +133,77 @@ function breaks(){
 
 
 
-
-
-
-  const app = new Vue({
-  el: '#app',
-  data() {
-    return {
-      client_id: '42f6854dd8ed4408a4ad3a7b52303250',
-      my_scopes:  '%2cugc-image-upload%2cuser-read-recently-played%2cuser-top-read%2cuser-read-playback-position%2cuser-read-playback-state%2cuser-modify-playback-state%2cuser-read-currently-playing%2capp-remote-control%2cstreaming%2cplaylist-modify-public%2cplaylist-modify-private%2cplaylist-read-private%2cplaylist-read-collaborative%2cuser-follow-modify%2cuser-follow-read%2cuser-library-modify%2cuser-library-read%2c',
-      redirect_uri: 'https://alexanderswann.github.io/DSA/',
-      me: null,
-      top: null,
-      my_token: null
-    }
-  },
-  methods: {
-    login() {
-      var popup = window.open(`https://accounts.spotify.com/authorize?client_id=${this.client_id}&response_type=token&redirect_uri=${this.redirect_uri}&scope=${this.my_scopes}&show_dialog=true`, 'Login with Spotify', 'width=800,height=600')
-
-      window.spotifyCallback = (payload) => {
-        //alert(payload)
-
-        this.my_token = payload
-
-        popup.close()
-
-        fetch('https://api.spotify.com/v1/me', {
-          headers: {
-            'Authorization': `Bearer ${payload}`
-          }
-        }).then(response => {
-          return response.json()
-        }).then(data => {
-          this.me = data
-        })
-      }
+const app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            client_id: '42f6854dd8ed4408a4ad3a7b52303250',
+            my_scopes: '%2cugc-image-upload%2cuser-read-recently-played%2cuser-top-read%2cuser-read-playback-position%2cuser-read-playback-state%2cuser-modify-playback-state%2cuser-read-currently-playing%2capp-remote-control%2cstreaming%2cplaylist-modify-public%2cplaylist-modify-private%2cplaylist-read-private%2cplaylist-read-collaborative%2cuser-follow-modify%2cuser-follow-read%2cuser-library-modify%2cuser-library-read%2c',
+            redirect_uri: 'https://alexanderswann.github.io/DSA/',
+            me: null,
+            top: null,
+            my_token: null
+        }
     },
+    methods: {
+        login() {
+            var popup = window.open(`https://accounts.spotify.com/authorize?client_id=${this.client_id}&response_type=token&redirect_uri=${this.redirect_uri}&scope=${this.my_scopes}&show_dialog=true`, 'Login with Spotify', 'width=800,height=600')
 
-    topsongs() {
-      //alert(this.my_token)
-      fetch('https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=50', {
-        headers: {
-          'Authorization': `Bearer ${this.my_token}`//'Bearer BQChB1cyLGz66SZqax2CM6lpEG8jFDHppZtDxmlzNtyz_LIlNIAu5qWKRMC6tMwkK5WE4z_GfO6SHEcywknltbQf9lZc_6DE9PsT6A1V_BmBW2pvzAMWFrM8lPHvHCjrFcIQMrLaPEwf8c5KAv4SsYY7LtNXY0oBFqDQ_C2cmoJIYQgXSfhrG0_xO7nLV0Vgqt8xngfN19XoJQxny9bDdT4q4SoHVBYlsDITQgwtKNbdTUvIy6HohmP4wtq5hH_vD9vvR81WLsznuFjJh5QLlJoTBFI'
+            window.spotifyCallback = (payload) => {
+                //alert(payload)
+
+                this.my_token = payload
+
+                popup.close()
+
+                fetch('https://api.spotify.com/v1/me', {
+                    headers: {
+                        'Authorization': `Bearer ${payload}`
+                    }
+                }).then(response => {
+                    return response.json()
+                }).then(data => {
+                    this.me = data
+                })
+            }
+        },
+
+        topsongs() {
+            //alert(this.my_token)
+            fetch('https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=50', {
+                headers: {
+                    'Authorization': `Bearer ${this.my_token}` //'Bearer BQChB1cyLGz66SZqax2CM6lpEG8jFDHppZtDxmlzNtyz_LIlNIAu5qWKRMC6tMwkK5WE4z_GfO6SHEcywknltbQf9lZc_6DE9PsT6A1V_BmBW2pvzAMWFrM8lPHvHCjrFcIQMrLaPEwf8c5KAv4SsYY7LtNXY0oBFqDQ_C2cmoJIYQgXSfhrG0_xO7nLV0Vgqt8xngfN19XoJQxny9bDdT4q4SoHVBYlsDITQgwtKNbdTUvIy6HohmP4wtq5hH_vD9vvR81WLsznuFjJh5QLlJoTBFI'
+
+                }
+            }).then(response => {
+                return response.json()
+            }).then(data => {
+                printtop(data)
+
+
+
+            })
+
+
+
+
+            //fetch('https://accounts.spotify.com/api/token?grant_type=authorization_code&code=' + this.my_token + '&redirect_uri=' + this.redirect_uri + '&client_id=' + this.client_id + '&client_secret=' + '90cf7eab479e4773945085484f3c2df4').then(response => {return response.json()}).then(data2 => {this.top = data2})
+            //alert('https://accounts.spotify.com/api/token?grant_type=authorization_code&code=' + this.my_token + '&redirect_uri=' + this.redirect_uri + '&client_id=' + this.client_id + '&client_secret=' + '90cf7eab479e4773945085484f3c2df4')
+            //this.top = this.me
 
         }
-      }).then(response => {
-        return response.json()
-      }).then(data => {
-        printtop(data)
+
+
+    },
+    mounted() {
+        this.token = window.location.hash.substr(1).split('&')[0].split("=")[1]
+
+        if (this.token) {
+            //alert(this.token)
 
 
 
-      })
-
-
-
-
-
-
-
-
-
-
-
-      //fetch('https://accounts.spotify.com/api/token?grant_type=authorization_code&code=' + this.my_token + '&redirect_uri=' + this.redirect_uri + '&client_id=' + this.client_id + '&client_secret=' + '90cf7eab479e4773945085484f3c2df4').then(response => {return response.json()}).then(data2 => {this.top = data2})
-      //alert('https://accounts.spotify.com/api/token?grant_type=authorization_code&code=' + this.my_token + '&redirect_uri=' + this.redirect_uri + '&client_id=' + this.client_id + '&client_secret=' + '90cf7eab479e4773945085484f3c2df4')
-      //this.top = this.me
-
+            window.opener.spotifyCallback(this.token)
+        }
     }
-
-
-  },
-  mounted() {
-    this.token = window.location.hash.substr(1).split('&')[0].split("=")[1]
-
-    if (this.token) {
-      //alert(this.token)
-
-
-
-      window.opener.spotifyCallback(this.token)
-    }
-  }
 })
