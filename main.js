676efdd016da3e31d77bdfff5c9b21c4c7ac7d50
alexanderswann <32579236+alexanderswann@users.noreply.github.com>
2021-04-22 20:58:51 -0400

    m

--- main.js
+++ main.js
@@ -71,6 +71,20 @@ function topsongs(){
         this.top = data
 
       })
+        dataout()
+      
+
+      }
+
+
+
+      //fetch('https://accounts.spotify.com/api/token?grant_type=authorization_code&code=' + this.my_token + '&redirect_uri=' + this.redirect_uri + '&client_id=' + this.client_id + '&client_secret=' + '90cf7eab479e4773945085484f3c2df4').then(response => {return response.json()}).then(data2 => {this.top = data2})
+      //alert('https://accounts.spotify.com/api/token?grant_type=authorization_code&code=' + this.my_token + '&redirect_uri=' + this.redirect_uri + '&client_id=' + this.client_id + '&client_secret=' + '90cf7eab479e4773945085484f3c2df4')
+      //this.top = this.me
+
+    },
+
+    dataout(){
       alert(this.top)
 
       this.top = JSON.parse(this.top)
@@ -87,15 +101,6 @@ function topsongs(){
         alert(title)
         alert(img)
         alert(artists)
-
-      }
-
-
-
-      //fetch('https://accounts.spotify.com/api/token?grant_type=authorization_code&code=' + this.my_token + '&redirect_uri=' + this.redirect_uri + '&client_id=' + this.client_id + '&client_secret=' + '90cf7eab479e4773945085484f3c2df4').then(response => {return response.json()}).then(data2 => {this.top = data2})
-      //alert('https://accounts.spotify.com/api/token?grant_type=authorization_code&code=' + this.my_token + '&redirect_uri=' + this.redirect_uri + '&client_id=' + this.client_id + '&client_secret=' + '90cf7eab479e4773945085484f3c2df4')
-      //this.top = this.me
-
     }
 
 
