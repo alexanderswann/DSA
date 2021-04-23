a7fb8778aa36f73c85c36c250e341ccb8c038b4d
alexanderswann <32579236+alexanderswann@users.noreply.github.com>
2021-04-22 21:23:08 -0400

    s

--- main.js
+++ main.js
@@ -13,11 +13,7 @@
 //
 // }
 //
-function topsongs(){
 
-  var url_code = "https://accounts.spotify.com/api/token?grant_type=authorization_code" + "&code=" + app.me + "&redirect_uri=" + REDIRECTURL + "&client_id=" + CLIENTID + "&client_secret=" + CLIENTSECRET;
-  loadJSON(url_code);
-}
 
 
 
@@ -85,6 +81,7 @@ function topsongs(){
 
     },
     dataout(){
+      alert("hello")
       alert(this.top)
       var title
       var img
