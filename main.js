cfa6a60048af8952fbff8e4d68eea4d76214a863
alexanderswann <32579236+alexanderswann@users.noreply.github.com>
2021-04-22 22:18:30 -0400

    a

--- main.js
+++ main.js
@@ -5,21 +5,11 @@
 // var REDIRECTURL = "https://www.spotify.com/us/home/";
 // var scopes = "%2cugc-image-upload%2cuser-read-recently-played%2cuser-top-read%2cuser-read-playback-position%2cuser-read-playback-state%2cuser-modify-playback-state%2cuser-read-currently-playing%2capp-remote-control%2cstreaming%2cplaylist-modify-public%2cplaylist-modify-private%2cplaylist-read-private%2cplaylist-read-collaborative%2cuser-follow-modify%2cuser-follow-read%2cuser-library-modify%2cuser-library-read%2c";
 // var refresh = "AQAzBYsvuKvMUj9bwTlghZal_d07EO9HHW4rblI-_aypVs31vJ89qdcHwwTmi0jfVJhQop9frRtilFFrFGaEU8-zdf9blr71qP8-BIWOzvgLxGY6pZi1ZGZWOSDqG7GrDmo";
-//
+var datatopsongs = null;
 // function setup(){
-
-      var client_id = '42f6854dd8ed4408a4ad3a7b52303250';
-      var my_scopes =  '%2cugc-image-upload%2cuser-read-recently-played%2cuser-top-read%2cuser-read-playback-position%2cuser-read-playback-state%2cuser-modify-playback-state%2cuser-read-currently-playing%2capp-remote-control%2cstreaming%2cplaylist-modify-public%2cplaylist-modify-private%2cplaylist-read-private%2cplaylist-read-collaborative%2cuser-follow-modify%2cuser-follow-read%2cuser-library-modify%2cuser-library-read%2c';
-      var redirect_uri = 'https://alexanderswann.github.io/DSA/';
-    var   me = null;
-    var   top = null;
-    var   my_token = null;
-
-    var login2 = select('#login');
-  	login2.mousePressed(login);
 //
-    var button = select('#topsongs');
-  	button.mousePressed(topsongs);
+//     var button = select('#topsongs');
+//   	button.mousePressed(topsongs);
 //
 // }
 //
@@ -30,42 +20,52 @@
 
 
 
-
-
-
-    function login() {
-      alert("hello");
-      let popup = window.open('https://accounts.spotify.com/authorize?client_id=' +client_id + '&response_type=token&redirect_uri=' +redirect_uri+ '&scope=' + my_scopes + '&show_dialog=true', 'Login with Spotify', 'width=800,height=600');
+  const app = new Vue({
+  el: '#app',
+  data() {
+    return {
+      client_id: '42f6854dd8ed4408a4ad3a7b52303250',
+      my_scopes:  '%2cugc-image-upload%2cuser-read-recently-played%2cuser-top-read%2cuser-read-playback-position%2cuser-read-playback-state%2cuser-modify-playback-state%2cuser-read-currently-playing%2capp-remote-control%2cstreaming%2cplaylist-modify-public%2cplaylist-modify-private%2cplaylist-read-private%2cplaylist-read-collaborative%2cuser-follow-modify%2cuser-follow-read%2cuser-library-modify%2cuser-library-read%2c',
+      redirect_uri: 'https://alexanderswann.github.io/DSA/',
+      me: null,
+      top: null,
+      my_token: null
+    }
+  },
+  methods: {
+    login() {
+      let popup = window.open(`https://accounts.spotify.com/authorize?client_id=${this.client_id}&response_type=token&redirect_uri=${this.redirect_uri}&scope=${this.my_scopes}&show_dialog=true`, 'Login with Spotify', 'width=800,height=600')
 
       window.spotifyCallback = (payload) => {
-        alert(payload);
+        alert(payload)
 
-        this.my_token = payload;
+        this.my_token = payload
 
-        popup.close();
+        popup.close()
 
         fetch('https://api.spotify.com/v1/me', {
           headers: {
-            'Authorization': 'Bearer ' + payload
+            'Authorization': `Bearer ${payload}`
           }
         }).then(response => {
           return response.json()
         }).then(data => {
           this.me = data
-        });
+        })
       }
-    }
+    },
 
-    function topsongs() {
+    topsongs() {
       alert(this.my_token)
       fetch('https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=5', {
         headers: {
-          'Authorization': 'Bearer ' + my_token
+          'Authorization': `Bearer ${this.my_token}`
         }
       }).then(response => {
         return response.json()
       }).then(data => {
         this.top = data
+        datatopsongs = data
 
       })
       dataout()
@@ -80,9 +80,8 @@
       //alert('https://accounts.spotify.com/api/token?grant_type=authorization_code&code=' + this.my_token + '&redirect_uri=' + this.redirect_uri + '&client_id=' + this.client_id + '&client_secret=' + '90cf7eab479e4773945085484f3c2df4')
       //this.top = this.me
 
-    }
-
-  function  dataout(){
+    },
+    dataout(){
       alert("hello")
       alert(this.top)
       var title
@@ -107,12 +106,11 @@
 
 
 
-
-
-
   }
 
-  function mounted() {
+
+  },
+  mounted() {
     this.token = window.location.hash.substr(1).split('&')[0].split("=")[1]
 
     if (this.token) {
@@ -123,3 +121,4 @@
       window.opener.spotifyCallback(this.token)
     }
   }
+})
