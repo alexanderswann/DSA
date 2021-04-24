1006df0d1b655b13811a82c24bb8e21f7c013c2f
alexanderswann <32579236+alexanderswann@users.noreply.github.com>
2021-04-23 21:50:51 -0400

    s

--- main.js
+++ main.js
@@ -59,7 +59,7 @@ function printtop(top) {
 }
 
 function playlistMaker(trackuri){
- var url = 'https://api.spotify.com/v1/users/' + "aksatl" + '/playlists';
+ var url = 'https://api.spotify.com/v1/users/' + user_id + '/playlists';
  fetch(url, {
     method: 'POST',
      headers: {
