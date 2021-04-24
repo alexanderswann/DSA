d592e56ed4722be4cb973ca0fd7ccbf4b2c166e4
alexanderswann <32579236+alexanderswann@users.noreply.github.com>
2021-04-23 20:34:23 -0400

    s

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 // var scopes = "%2cugc-image-upload%2cuser-read-recently-played%2cuser-top-read%2cuser-read-playback-position%2cuser-read-playback-state%2cuser-modify-playback-state%2cuser-read-currently-playing%2capp-remote-control%2cstreaming%2cplaylist-modify-public%2cplaylist-modify-private%2cplaylist-read-private%2cplaylist-read-collaborative%2cuser-follow-modify%2cuser-follow-read%2cuser-library-modify%2cuser-library-read%2c";
 // var refresh = "AQAzBYsvuKvMUj9bwTlghZal_d07EO9HHW4rblI-_aypVs31vJ89qdcHwwTmi0jfVJhQop9frRtilFFrFGaEU8-zdf9blr71qP8-BIWOzvgLxGY6pZi1ZGZWOSDqG7GrDmo";
 var datatopsongs = '';
-var user_token = "";
+var user_token = "BQCuLZlbDsstf5kxIL6VTlSvYtXU5DWUdW3tWkBy_YYsCPt0LVusBX_GgJi1kn5L1ZWos3LvulWl9vYXwPKajF4qFdw7z1DTRQSnHodiAH40A686j3nUMOw5nreSBoegjuHNgXknQ-esY5TzDbm1wleOEmbXpjh9M7CCmIjW8JVVkINWcobp1U9NxOYKBI2Z8GyyQ8SM58kvILGjNhZ5R5BjzUpz1ukJOgKn7RnNSJg97GXcuClTVM4lwNXSAyY1Uwdpl_1KtOijKZOGdiGOS1WfmpU";
 var user_id = "";
 // function setup(){
 //
@@ -61,12 +61,13 @@ function printtop(top) {
 function playlistMaker(trackuri){
  var url = 'https://api.spotify.com/v1/users/' + "aksatl" + '/playlists';
  fetch(url, {
+    method: 'POST',
      headers: {
          'Authorization': 'Bearer ' + user_token,
          "Accept": "application/json",
          "Content-Type": "application/json"
      },
-     body: JSON.stringify("{\"name\":\"Poopy Doopy\",\"description\":\"New playlist description\",\"public\":false}")
+     body: '{"name": "New Playlist","description": "New fucking playlist description","public": false}',
  }).then(response => {
      return response.json()
  }).then(data => {
@@ -190,7 +191,7 @@ const app = new Vue({
             redirect_uri: 'https://alexanderswann.github.io/DSA/',
             me: null,
             top: null,
-            my_token: null
+            my_token: "BQCuLZlbDsstf5kxIL6VTlSvYtXU5DWUdW3tWkBy_YYsCPt0LVusBX_GgJi1kn5L1ZWos3LvulWl9vYXwPKajF4qFdw7z1DTRQSnHodiAH40A686j3nUMOw5nreSBoegjuHNgXknQ-esY5TzDbm1wleOEmbXpjh9M7CCmIjW8JVVkINWcobp1U9NxOYKBI2Z8GyyQ8SM58kvILGjNhZ5R5BjzUpz1ukJOgKn7RnNSJg97GXcuClTVM4lwNXSAyY1Uwdpl_1KtOijKZOGdiGOS1WfmpU"
         }
     },
     methods: {
