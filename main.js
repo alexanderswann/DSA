e8c04ad15062e2eb86e1ed42252fee8625c8228c
alexanderswann <32579236+alexanderswann@users.noreply.github.com>
2021-04-26 10:56:14 -0400

    a

--- main.js
+++ main.js
@@ -248,7 +248,7 @@ const app = new Vue({
     data() {
         return {
             client_id: '42f6854dd8ed4408a4ad3a7b52303250',
-            my_scopes: '%2cugc-image-upload%2cuser-read-recently-played%2cuser-top-read%2cuser-read-playback-position%2cuser-read-playback-state%2cuser-modify-playback-state%2cuser-read-currently-playing%2capp-remote-control%2cstreaming%2cplaylist-modify-public%2cplaylist-modify-private%2cplaylist-read-private%2cplaylist-read-collaborative%2cuser-follow-modify%2cuser-follow-read%2cuser-library-modify%2cuser-library-read%2c',
+            my_scopes: '%2cugc-image-upload%2cuser-read-recently-played%2cuser-top-read%2cplaylist-modify-public%2cplaylist-modify-private%2cplaylist-read-private%2cplaylist-read-collaborative%2cuser-library-modify%2cuser-library-read%2c',
             redirect_uri: 'https://alexanderswann.github.io/DSA/',
             me: null,
             top: null,
