e22fa255e3e71a69947a595ca862e23e27fb2088
alexanderswann <32579236+alexanderswann@users.noreply.github.com>
2021-04-29 13:01:03 -0400

    w

--- main.js
+++ main.js
@@ -282,7 +282,7 @@ const app = new Vue({
 
         topsongs() {
             //alert(this.my_token)
-            this.top = "please wait while your top songs and your new playlist are being generated. Check your spotify account in about 30 seconds."
+            this.top = "Please wait while your top songs and your new playlist are being generated. Check your spotify account in about 30 seconds."
             token(this.my_token)
             fetch('https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=50', {
                 headers: {
